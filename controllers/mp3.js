b3c09c3a002c04af18abead0e7165aa7eea3b7b5
khanhdayr002 <154343426+khanhdayr002@users.noreply.github.com>
2024-05-15 23:23:18 +0700

    Update mp3.js

--- controllers/mp3.js
+++ controllers/mp3.js
@@ -3,5 +3,5 @@ const { errorHandler } = require("../utils");
 exports.mp3 = async (req, res, next) => {
   var data = require('fs-extra').readFileSync(__dirname + '/../mp3.txt', 'utf-8').split('\n');
   link = data[Math.floor(Math.random() * data.length)].trim();
-  res.json({"Author":"mhieu","data":`${link}`});
+  res.json({"Author":"Gkhanh","data":`${link}`});
 };
